7483ec4d3a8fffba8a9f02678f33493a95dc6492
yaroslawmoskalenko <45899296+yaroslawmoskalenko@users.noreply.github.com>
2018-12-15 22:56:20 +0300

    Fistr commit
    Bookand rest C++

---
+++ 25-Bookshop/25-Bookshop/25-Bookshop.cpp
@@ -0,0 +1,34 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+
+#include "pch.h"
+//#include <stdafx.h>
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+
+using namespace std;
+int main(int argc, char* argv[])
+{
+	float a, b, y;
+	cout << "Your money a=";
+	cin >> a;
+	cout << "Book cost b=";
+	cin >> b;
+	if (a == b)
+		cout << "Thank you for buying our book, come here later ;)";
+	if (a > b)
+	{
+		y = a - b;
+		cout << "Thank you for buying our book, come here later, take your rest: ";
+		cout << y << endl;
+	}
+	if (a < b)
+	{
+		y = b - a;
+		cout << "Please, pay more: ";
+		cout << y << endl;
+	}
+	system("pause");
+}
