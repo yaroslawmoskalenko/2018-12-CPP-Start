4b0f3efc2dd2ad8e2d646fc9747e28cbbbf3892b
yaroslawmoskalenko <45899296+yaroslawmoskalenko@users.noreply.github.com>
2018-12-16 15:45:22 +0300

    Simple input, if, else...

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿
+
+#include "pch.h"
+#include <iostream>
+#include <cmath>
+#include <stdio.h>
+
+using namespace std;
+int main(int argc, char* argv[])
+{
+	float a, y, x;
+	cout << "Enter x="; cin >> x;
+
+		if (x >= 0) {
+			cout << "Enter a="; cin >> a;
+			if (a*a*a - 1 != 0) {
+				y = (sqrt(x) + a) / (a*a*a - 1);
+				cout << "y=" << y << endl;
+			} else {
+				cout << "Wrong input a" << endl; 
+			}
+		} else {
+			cout << "Wrong input x" << endl; 
+		}
+	system("pause"); 
+	
+
+}
+
+
