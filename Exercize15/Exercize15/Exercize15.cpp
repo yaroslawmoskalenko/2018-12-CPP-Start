5968a81222487c46f4e2683f5c40058ef529d25c
yaroslawmoskalenko <45899296+yaroslawmoskalenko@users.noreply.github.com>
2019-02-07 20:41:34 +0300

    Ex15

---
+++ Exercize15/Exercize15/Exercize15.cpp
@@ -0,0 +1,29 @@
+﻿// Exercize15.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+int Date(int D, int M, int Y)
+{
+	int a, y, m, R;
+	a = (14 - M) / 12;
+	y = Y - a;
+	m = M + 12 * a - 2;
+	R = 7000 + D + y + y / 4 - y / 100 + y / 400 + 31 * m / 12;
+	return R % 7;
+}
+int main()
+{
+	int D, M, Y;
+	cout << "D="; cin >> D;
+	cout << "M="; cin >> M;
+	cout << "Y="; cin >> Y;
+	string S[7] = { "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" };
+	cout << S[Date(D, M, Y)] << endl;
+	system("pause");
+	return 0;
+}
+
---
+++ ProperProject/ProperProject/ProperProject.cpp
@@ -0,0 +1,17 @@
+﻿
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+int main(int argc, char* argv[])
+{
+	int DadsMassive[5];
+	for (int t = 0; t < 5; t++) {
+		
+		cout << "DadsMassive1=" << endl; cin >> DadsMassive[0];
+	}
+	for (int f = 0; f < 5; f++) {
+
+
+
+}
