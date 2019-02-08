2fa5dbf016cace52987ab128afba469405b75e8a
yaroslawmoskalenko <45899296+yaroslawmoskalenko@users.noreply.github.com>
2019-02-08 21:36:55 +0300

    Ex.16

---
+++ Exercize16/Exercize16/Exercize16.cpp
@@ -0,0 +1,34 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	int n, Y;
+	int MounthsDays[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	int StartDayOfMonths[12];
+	string MonthsNames[12] = { "JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC" };
+	string dayOfWeek[7] = { "SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT" };
+	cout << "Y="; cin >> Y;
+	cout << "n="; cin >> n;
+	bool isLeapYear = (Y % 400 == 0) && (Y % 4 == 0) && (Y % 100 != 0);
+	int startday = 1;
+	for (int i = 0; i < 12; i++) {
+		StartDayOfMonths[i] = startday;
+		startday = startday + MounthsDays[i];
+		if (isLeapYear && i == 1)
+			startday++;
+		//cout << StartDayOfMonths[i] << endl;
+	}
+	int i;
+	for (i = 0; i < 12; i++) {
+		if (StartDayOfMonths[i] > n)
+			break;
+	}
+	i--;
+	//cout << "i=" << i << endl;
+	cout << dayOfWeek[n % 7] << endl;
+	cout << MonthsNames[i] << endl;
+
+}
