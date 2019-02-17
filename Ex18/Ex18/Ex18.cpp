c7ca6ebdfddc7a8bc4f66e3da3cbd1f23ee2611b
yaroslawmoskalenko <45899296+yaroslawmoskalenko@users.noreply.github.com>
2019-02-17 23:33:37 +0300

    Ex18

---
+++ Ex18/Ex18/Ex18.cpp
@@ -0,0 +1,35 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+using namespace std;
+float lenght(float x1, float y1, float x2, float y2) {
+	return sqrt((x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1));
+
+}
+int main(int argc, char* argv[])
+{
+	float X[5];
+	float Y[5];
+	for (int i = 0; i < 5; i++) {
+		cout << "Input x="; cin >> X[i];
+		cout << "Input y="; cin >> Y[i];
+	}
+	float max = -1;
+	float min = lenght(X[0], Y[0], X[1], Y[1]);
+	for (int i = 0; i < 5; i++) {
+		float L = lenght(X[i], Y[i], X[(i + 1) % 5], Y[(i + 1) % 5]);
+		if (L > max)
+			max = L;
+		if (L < min)
+			min = L;
+	}
+	cout << "The longest is= " << max << endl;
+	cout << "The shortest is= " << min << endl;
+
+}
+
+	
+
+
---
+++ Exercize17/Exercize17/Exercize17.cpp
@@ -0,0 +1,36 @@
+﻿
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	float x1, x2, x3, x4, y1, y2, y3, y4;
+	cout << "x1="; cin >> x1;
+	cout << "y1="; cin >> y1;
+	cout << "x2="; cin >> x2;
+	cout << "y2="; cin >> y2;
+	cout << "x3="; cin >> x3;
+	cout << "y3="; cin >> y3;
+	cout << "x4="; cin >> x4;
+	cout << "y4="; cin >> y4;
+
+	float xa, ya, xb, yb, al, bl;
+	xa = x2 - x1;
+	ya = y2 - y1;
+	xb = x4 - x3;
+	yb = y4 - y3;
+	al = sqrt(xa * xa + ya * ya);
+	bl = sqrt(xb * xb + yb * yb);
+
+	if (xa * xb + ya * yb == 0)
+		cout << "Vectors are perpendicular" << endl;
+	if (xa / xb == ya / yb)
+		cout << "Vectors are collinear" << endl;
+	if (xa / xb == ya / yb && al == bl)
+		cout << "Vectors are equal" << endl;
+
+
+}
+
