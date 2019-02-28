a428d4a14a1cba79d4c7e6f98dc59c5e89c25b06
yaroslawmoskalenko <45899296+yaroslawmoskalenko@users.noreply.github.com>
2019-02-28 23:01:16 +0300

    Ex. 20

---
+++ Ex19/Ex19/Ex19.cpp
@@ -0,0 +1,26 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+
+using namespace std;
+int main(int argc, char* argv[])
+{ 
+	int n, Y;
+	int MounthsDays[12] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	cout << "Y="; cin >> Y;
+	cout << "n="; cin >> n;
+	if (n < 1 || n > 12 || Y > 9999)
+		cout << "incorrect n or Y!";
+	bool isLeapYear = (Y % 400 == 0) && (Y % 4 == 0) && (Y % 100 != 0);
+	/*if (n = 1)
+		cout << "Days=" << MounthsDays[0] << endl;*/
+	if (isLeapYear)
+		MounthsDays[1] = 29;
+	if (n = 1) 
+		cout << "Days=" << MounthsDays[0] << endl;
+
+	 
+}
+
---
+++ Exercize 20/Exercize 20/Exercize 20.cpp
@@ -0,0 +1,38 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	float x;
+	cout << "Input mark = "; cin >> x;
+	if (x <= 0 || x > 12)
+		cout << "Wrong input" << endl;
+	else if (x = 1)
+		cout << "Pogano, One" << endl;
+	else if (x = 2)
+		cout << "Nezadovilno, Two" << endl;
+	else if (x = 3)
+		cout << "Zadovilno, Tree" << endl;
+	else if (x = 4)
+		cout << "Dobre, Four" << endl;
+	else if (x = 5)
+		cout << "Widminno, Five" << endl;
+	else if (x = 6)
+		cout << "Six" << endl;
+	else if (x = 7)
+		cout << "Seven" << endl;
+	else if (x = 8)
+		cout << "Eight" << endl;
+	else if (x = 9)
+		cout << "Nine" << endl;
+	else if (x = 10)
+		cout << "Ten" << endl;
+	else if (x = 11)
+		cout << "Eleven" << endl;
+	else if (x = 12)
+		cout << "Twelve" << endl;
+}
+
