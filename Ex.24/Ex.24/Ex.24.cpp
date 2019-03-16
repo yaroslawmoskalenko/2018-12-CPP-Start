2adb04d987d83ad4b075a94f84090789c1674771
yaroslawmoskalenko <45899296+yaroslawmoskalenko@users.noreply.github.com>
2019-03-16 19:46:47 +0300

    Ex.24

---
+++ Ex.24/Ex.24/Ex.24.cpp
@@ -0,0 +1,21 @@
+﻿
+#include "pch.h"
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+using namespace std;
+int main(int argc, char* argv[])
+{
+	float a, b;
+	cout << "Enter the birth year = "; cin >> a;
+	cout << "Enter the year of today = "; cin >> b;
+	float y = a - b;
+	if (y <= 6)
+		cout << "Child" << y << endl;
+	if (y > 6 && y < 18)
+		cout << "Teenager" << y << endl;
+	if (y >= 18 && y < 60)
+		cout << "Working man" << y << endl;
+	if (y >= 60)
+		cout << "Old man" << y << endl;
+}
